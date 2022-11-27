0e9b1e7825db6dbfd3e130bf802780f522646559
xhoa <send2me2nd@gmail.com>
2022-11-27 20:51:14 +0100

    New SpringBoot Project

---
+++ Udemy/SpringBoot/src/com/luv2code/springdemo/BaseballCoach.java
@@ -0,0 +1,12 @@
+package com.luv2code.springdemo;
+
+public class BaseballCoach implements Coach{
+	
+	@Override
+	public String getDailyWorkout() {
+		
+		return  "Spend 1h on batting practive. ";
+		
+	}
+
+}
