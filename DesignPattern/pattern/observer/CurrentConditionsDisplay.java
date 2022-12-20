e387e72cc2e1a14afd02e16fd179893f45550208
xhoa <send2me2nd@gmail.com>
2022-12-20 10:47:56 +0100

    added new Displays

--- DesignPattern/pattern/observer/CurrentConditionsDisplay.java
+++ DesignPattern/pattern/observer/CurrentConditionsDisplay.java
@@ -19,9 +19,9 @@ public class CurrentConditionsDisplay implements Observer, DisplayElements {
     }
 
     @Override
-    public void update(float temperature, float humidity, float pressure) {
-        this.temperature = temperature;
-        this.humidity = humidity;
+    public void update() {
+        this.temperature = weatherData.getTemperature();
+        this.humidity = weatherData.getHumidity();
         display();
 
     }
---
+++ DesignPattern/pattern/observer/ForecastDisplay.java
@@ -0,0 +1,35 @@
+package pattern.observer;
+
+public class ForecastDisplay implements Observer, DisplayElements {
+
+    private WeatherData weatherData;
+    private float currentPressure = 29.92f;
+    private float lastPressure;
+
+    public ForecastDisplay(WeatherData weatherData) {
+        this.weatherData = weatherData;
+        weatherData.registerObserver(this);
+    }
+
+    @Override
+    public void display() {
+        System.out.println("Forecast: ");
+        if (currentPressure > lastPressure) {
+            System.out.println("Improving weather on the way!");
+        } else if (currentPressure == lastPressure) {
+            System.out.println("More of the same.");
+        } else if (currentPressure < lastPressure) {
+            System.out.println("Watch out for cooler, rainy weather.");
+        }
+
+    }
+
+    @Override
+    public void update() {
+        lastPressure = currentPressure;
+        currentPressure = weatherData.getPressure();
+        display();
+
+    }
+
+}
