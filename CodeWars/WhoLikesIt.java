b062fbeee65dd848380649901823d984c9d19eb6
xhoa <send2me2nd@gmail.com>
2023-01-14 00:37:40 +0100

    ..

--- CodeWars/WhoLikesIt.java
+++ CodeWars/WhoLikesIt.java
@@ -18,10 +18,16 @@ class WhoLikesIt {
 
     public static String whoLikesIt_best(String... names) {
         switch (names.length) {
-          case 0: return "no one likes this";
-          case 1: return String.format("%s likes this", names[0]);
-          case 2: return String.format("%s and %s like this", names[0], names[1]);
-          case 3: return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
-          default: return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
+            case 0:
+                return "no one likes this";
+            case 1:
+                return String.format("%s likes this", names[0]);
+            case 2:
+                return String.format("%s and %s like this", names[0], names[1]);
+            case 3:
+                return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+            default:
+                return String.format("%s, %s and %d others like this", names[0], names[1], names.length - 2);
         }
+    }
 }
